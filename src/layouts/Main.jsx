7aafededde17788d245c17db6f26a3e405d25cf2
tryit-100 <safwan.std21@gmail.com>
2023-11-08 22:45:12 +0600

    react-router with tailwind initiaded and main routes setup is done

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h2 className='text-5xl'>Main Layout</h2>
+        </div>
+    );
+};
+
+export default Main;
